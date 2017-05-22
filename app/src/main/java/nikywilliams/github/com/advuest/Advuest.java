745b372ed1df7bef87d80267fbf89dc911e3959f
Nik <nikywilliams@gmail.com>
2017-05-22 14:49:10 -0500

    Initial Commit

---
+++ app/src/main/java/nikywilliams/github/com/advuest/Advuest.java
@@ -0,0 +1,13 @@
+package nikywilliams.github.com.advuest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Advuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advuest);
+    }
+}
